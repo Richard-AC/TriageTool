bdb31c07cdd1995782f43d85ea9f21b3727722d7
Richard-AC <richard_abou_chaaya@brown.edu>
2021-07-20 14:43:50 +0200

    Printing context information upon Debug Event

--- TriageTool.cpp
+++ TriageTool.cpp
@@ -1,10 +1,11 @@
 #include <windows.h>
 #include <stdio.h>
+#include <atlstr.h>
 
-#define DEBUG 1
+#define DBG 1
 
 #define debug_print(...) \
-            do { if (DEBUG) fprintf(stderr, __VA_ARGS__); } while (0)
+            do { if (DBG) fwprintf(stderr, __VA_ARGS__); } while (0)
 
 
 void EnterDebugLoop(const LPDEBUG_EVENT DebugEv);
@@ -40,17 +41,6 @@ int main(int argc, char* argv[]) {
     // Debugger loop 
     EnterDebugLoop(&DebugEv);
 
-    // Wait until child process exits.
-    WaitForSingleObject(pi.hProcess, INFINITE);
-
-    exit_code = 0;
-    GetExitCodeProcess(pi.hProcess, &exit_code);
-
-    printf("case %d exit code was %d\n", i, exit_code);
-    if (exit_code != 0) {
-        return 0;
-    }
-
     // Close process and thread handles. 
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
@@ -59,12 +49,32 @@ int main(int argc, char* argv[]) {
 
 void EnterDebugLoop(const LPDEBUG_EVENT DebugEv) {
     DWORD dwContinueStatus = DBG_CONTINUE; // exception continuation 
+    CONTEXT context = { 0 };
+    CString strRegisters;
+    HANDLE hThread;
     for (;;) {
         WaitForDebugEvent(DebugEv, INFINITE);
-        
+        hThread = OpenThread(THREAD_GET_CONTEXT, FALSE, DebugEv->dwThreadId);
+        if (hThread == NULL) {
+            printf("Failed to retreive thread handle from thread id. GetLastError: %d", GetLastError());
+            return;
+        }
+        context.ContextFlags = CONTEXT_ALL;
+        GetThreadContext(hThread, &context);
+        strRegisters.Format(
+            L"EAX = %08X EBX = %08X ECX = %08X\n"
+            L"EDX = %08X ESI = %08X EDI = %08X\n"
+            L"EIP = %08X ESP = %08X EBP = %08X\n"
+            L"EFL = %08X",
+            context.Eax, context.Ebx, context.Ecx,
+            context.Edx, context.Esi, context.Edi,
+            context.Eip, context.Esp, context.Ebp,
+            context.EFlags
+        );
+
         switch (DebugEv->dwDebugEventCode) {
         case EXCEPTION_DEBUG_EVENT:
-            debug_print("EXCEPTION_DEBUG_EVENT Exception Code: ");
+            debug_print(L"EXCEPTION_DEBUG_EVENT Exception Code: ");
             // Process the exception code. When handling 
             // exceptions, remember to set the continuation 
             // status parameter (dwContinueStatus). This value 
@@ -74,41 +84,44 @@ void EnterDebugLoop(const LPDEBUG_EVENT DebugEv) {
             case EXCEPTION_ACCESS_VIOLATION:
                 // First chance: Pass this on to the system. 
                 // Last chance: Display an appropriate error. 
-                debug_print("EXCEPTION_ACCESS_VIOLATION\n");
+                debug_print(L"EXCEPTION_ACCESS_VIOLATION\n");
+                debug_print(L"%s\n", (LPCTSTR) strRegisters);
                 dwContinueStatus = DBG_EXCEPTION_NOT_HANDLED;
                 break;
 
             case EXCEPTION_BREAKPOINT:
                 // First chance: Display the current 
                 // instruction and register values. 
-                debug_print("EXCEPTION_BREAKPOINT\n");
+                debug_print(L"EXCEPTION_BREAKPOINT\n");
+                debug_print(L"%s\n", (LPCTSTR) strRegisters);
                 dwContinueStatus = DBG_CONTINUE;
                 break;
 
             case EXCEPTION_DATATYPE_MISALIGNMENT:
                 // First chance: Pass this on to the system. 
                 // Last chance: Display an appropriate error. 
-                debug_print("EXCEPTION_DATATYPE_MISALIGNMENT\n");
+                debug_print(L"EXCEPTION_DATATYPE_MISALIGNMENT\n");
                 dwContinueStatus = DBG_EXCEPTION_NOT_HANDLED;
                 break;
 
             case EXCEPTION_SINGLE_STEP:
                 // First chance: Update the display of the 
                 // current instruction and register values. 
-                debug_print("EXCEPTION_SINGLE_STEPT\n");
+                debug_print(L"EXCEPTION_SINGLE_STEPT\n");
                 dwContinueStatus = DBG_CONTINUE;
                 break;
 
             case DBG_CONTROL_C:
                 // First chance: Pass this on to the system. 
                 // Last chance: Display an appropriate error. 
-                debug_print("EXCEPTION_DBG_CONTROL_C\n");
+                debug_print(L"EXCEPTION_DBG_CONTROL_C\n");
                 dwContinueStatus = DBG_EXCEPTION_NOT_HANDLED;
                 break;
 
             default:
                 // Handle other exceptions. 
-                debug_print("OTHER EXCEPTION\n");
+                debug_print(L"OTHER EXCEPTION\n");
+                debug_print(L"%s\n", (LPCTSTR) strRegisters);
                 dwContinueStatus = DBG_EXCEPTION_NOT_HANDLED;
                 break;
             }
@@ -120,7 +133,7 @@ void EnterDebugLoop(const LPDEBUG_EVENT DebugEv) {
             // with the GetThreadContext and SetThreadContext functions; 
             // and suspend and resume thread execution with the 
             // SuspendThread and ResumeThread functions. 
-            debug_print("CREATE_THREAD_DEBUG_EVENT\n");
+            debug_print(L"CREATE_THREAD_DEBUG_EVENT\n");
             break;
 
         case CREATE_PROCESS_DEBUG_EVENT:
@@ -132,18 +145,20 @@ void EnterDebugLoop(const LPDEBUG_EVENT DebugEv) {
             // thread execution with the SuspendThread and ResumeThread
             // functions. Be sure to close the handle to the process image
             // file with CloseHandle.
-            debug_print("CREATE_PROCESS_DEBUG_EVENT\n");
+            debug_print(L"CREATE_PROCESS_DEBUG_EVENT\n");
             break;
 
         case EXIT_THREAD_DEBUG_EVENT:
             // Display the thread's exit code. 
-            debug_print("EXIT_THREAD_DEBUG_EVENT\n");
+            debug_print(L"EXIT_THREAD_DEBUG_EVENT\n");
 
             break;
 
         case EXIT_PROCESS_DEBUG_EVENT:
             // Display the process's exit code. 
-            debug_print("EXIT_PROCESS_DEBUG_EVENT\n");
+            debug_print(L"EXIT_PROCESS_DEBUG_EVENT\n");
+            debug_print(L"Process exited with code:  0x%x", DebugEv->u.ExitProcess.dwExitCode);
+            return;
 
             break;
 
@@ -166,6 +181,7 @@ void EnterDebugLoop(const LPDEBUG_EVENT DebugEv) {
         case RIP_EVENT:
             break;
         }
+        CloseHandle(hThread);
         ContinueDebugEvent(DebugEv->dwProcessId,
             DebugEv->dwThreadId,
             dwContinueStatus);
